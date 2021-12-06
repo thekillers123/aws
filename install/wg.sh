a9aaf0b8c70df234612aacd48daadbb84b53e188
thekillers123 <azriff456@gmail.com>
2021-12-06 16:34:38 +0800

    Update wg.sh

--- install/wg.sh
+++ install/wg.sh
@@ -4,7 +4,7 @@
 # Centos 7 & 8 64bit 
 # Mod by SL
 # ==================================================
-GitUser="syapik96"
+GitUser="thekillers123"
 #wget https://github.com/${GitUser}/
 
 # Check OS version
@@ -90,11 +90,11 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O addwg "https://raw.githubusercontent.com/${GitUser}/syapik96/main/tambah/addwg.sh"
-wget -O delwg "https://raw.githubusercontent.com/${GitUser}/syapik96/main/hapus/delwg.sh"
-wget -O cekwg "https://raw.githubusercontent.com/${GitUser}/syapik96/main/cekwg.sh"
-wget -O xp-wg "https://raw.githubusercontent.com/${GitUser}/syapik96/main/xp-wg.sh"
-wget -O renewwg "https://raw.githubusercontent.com/${GitUser}/syapik96/main/renewwg.sh"
+wget -O addwg "https://raw.githubusercontent.com/thekillers123/main/tambah/addwg.sh"
+wget -O delwg "https://raw.githubusercontent.com/thekillers123/main/hapus/delwg.sh"
+wget -O cekwg "https://raw.githubusercontent.com/thekillers123/main/cekwg.sh"
+wget -O xp-wg "https://raw.githubusercontent.com/thekillers123/main/xp-wg.sh"
+wget -O renewwg "https://raw.githubusercontent.com/thekillers123/main/renewwg.sh"
 chmod +x addwg
 chmod +x delwg
 chmod +x cekwg
